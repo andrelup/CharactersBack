7bbcb517e01364cb52a3047c2e4cd01868a8e56d
andrelup <andrelcop@gmail.com>
2022-10-16 01:23:25 +0200

    clean code

--- models/users.js
+++ models/users.js
@@ -9,40 +9,16 @@ class Users {
 
   save() {
     const db = getDb();
-    return db
-      .collection("users")
-      .insertOne(this)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    return db.collection("users").insertOne(this);
   }
   edit() {
     const db = getDb();
     return db.collection("users").findOneAndUpdate({ email: this.email }, this);
-    //   .then((result) => {
-    //     console.log(result);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
   }
   static find(email) {
     console.log("EMAIL: ", email);
     const db = getDb();
-    return db
-      .collection("users")
-      .find({ email: email })
-      .toArray()
-      .then((user) => {
-        console.log(user);
-        return user;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    return db.collection("users").find({ email: email }).toArray();
   }
 }
 
