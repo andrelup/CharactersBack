93247ef7df446a617063a1d97f6f01d5a51ca2a0
andrelup <andrelcop@gmail.com>
2022-10-16 15:33:34 +0200

    add fetch library. build two routes for caracters

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const fetch = require("node-fetch");
 
-router.get("/characters", (req, res) => {
-  res.send("/characters URL Works");
+router.get("/characters", async (req, res) => {
+  let queryParams = req.query;
+  let url = new URL("https://rickandmortyapi.com/api/character");
+  if (queryParams) {
+    console.log("[characters] queryParams: ", queryParams);
+    Object.keys(queryParams).forEach((key) =>
+      url.searchParams.append(key, queryParams[key])
+    );
+  }
+  // console.log("URL: ", url);
+  fetch(url, {
+    method: "GET",
+    queryParams: queryParams,
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.text())
+    .then((text) => {
+      res.send(text);
+    });
 });
 router.get("/characters/:id", (req, res) => {
-  res.send(" /characters/:id URL Works");
+  const id = req.params.id;
+  fetch("https://rickandmortyapi.com/api/character/" + id, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.text())
+    .then((text) => {
+      console.log("GET character by id: ", text);
+      res.send(text);
+    });
 });
 
 module.exports = router;
