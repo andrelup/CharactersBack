a71e9ed0b20046a3303eef0d14ac5991e71bb7d1
andrelup <andrelcop@gmail.com>
2022-10-19 12:08:42 +0200

    add some comments

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+// import libraries
 const express = require("express");
-const mongoConnect = require("./util/database").mongoConnect;
 var bodyParser = require("body-parser");
 const cors = require("cors");
-const fs = require('fs');
-// import library and files
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
-// const customCss = fs.readFileSync((process.cwd() + "/swagger.css"), 'utf8');
-// let express to use this
-const app = express();
+
+// import need files
+const mongoConnect = require("./util/database").mongoConnect;
 const usersRoutes = require("./routes/users.routes");
 const charactersRoutes = require("./routes/characters.routes");
 const authRoutes = require("./routes/auth.routes");
 
+const app = express();
 app.use(cors());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(bodyParser.urlencoded({ extended: false }));
